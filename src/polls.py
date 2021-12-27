3e5a33d076f4aba015e783fb76bd4a3495f9f0b3
thatonecalculator <kainoakanter@gmail.com>
2021-12-26 22:16:32 -0800

    debug print

--- src/polls.py
+++ src/polls.py
@@ -132,6 +132,7 @@ class PollsCog(commands.Cog):
 	async def on_ready(self):
 		if not self.persistent_polls_added:
 			for i in polls.find():
+				print(i)
 				self.bot.add_view(self.PollView(
 					i["options"], i["title"], i["min_choices"], i["max_choices"], i["_id"]
 				))
